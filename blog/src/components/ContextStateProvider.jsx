8a2d016a60cc13fa508bb0cf0ac3e452429d5d8c
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-20 16:15:21 +0800

    loading is renewed

--- blog/src/components/ContextStateProvider.jsx
+++ blog/src/components/ContextStateProvider.jsx
@@ -1,71 +1,64 @@
-import {useState,createContext,useContext,useEffect} from 'react';
-import { fetchAllCategoryBlogs } from './Util/fetchBlogs';
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
-const InitialAllBlogsContext=createContext(null)
-export const useInitialAllBlogsContext=({categoryName,whichOne})=>{
-  const initialAllBlogs= useContext(InitialAllBlogsContext)
-  const object=initialAllBlogs?.find(blogObj=>blogObj.category_name==categoryName)
-  switch(whichOne)
-  {
-    case 'blogs':
-      {
-        return object?.blogs
-      }
-    case 'all_tags':
-      {
-        return object?.all_tags
-      }
+import { useState, createContext, useContext, useEffect } from "react";
+import { fetchAllCategoryBlogs } from "./Util/fetchBlogs";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+const InitialAllBlogsContext = createContext(null);
+export const useInitialAllBlogsContext = ({ categoryName, whichOne }) => {
+  const initialAllBlogs = useContext(InitialAllBlogsContext);
+  const object = initialAllBlogs?.find(
+    (blogObj) => blogObj.category_name == categoryName
+  );
+  switch (whichOne) {
+    case "blogs": {
+      return object?.blogs;
+    }
+    case "all_tags": {
+      return object?.all_tags;
+    }
   }
-}
-export function ContextStateProvider({children}){
-    const [allCategoryBlogs,setAllCategoryBlogs]=useState([])
-    console.log('all category obj',allCategoryBlogs)
-    useEffect(()=>{
-         fetchAllCategoryBlogs(setAllCategoryBlogs)
-       },[])
-  if(allCategoryBlogs.length>0)
-  {
-   if(allCategoryBlogs[0].blogs.length>0)
-   {
-    return (
-      <InitialAllBlogsContext.Provider value={allCategoryBlogs}>
+};
+export function ContextStateProvider({ children }) {
+  const [allCategoryBlogs, setAllCategoryBlogs] = useState([]);
+  console.log("all category obj", allCategoryBlogs);
+  useEffect(() => {
+    fetchAllCategoryBlogs(setAllCategoryBlogs);
+  }, []);
+  if (allCategoryBlogs.length > 0) {
+    if (allCategoryBlogs[0].blogs.length > 0) {
+      return (
+        <InitialAllBlogsContext.Provider value={allCategoryBlogs}>
           {children}
-      </InitialAllBlogsContext.Provider>)
-   }
-   else
-   {
-    return <div className="relative flex flex-col h-full ">
-
-    <div className="w-full z-10 bg-white sticky top-0 ">
-    <div className=" min-w-[240px] max-w-[1230px] mx-auto ">
-    <Header/>
-    </div>
-    </div>
-    <p className='text-center font-extrabold '>No internet connection</p>
-    <div className="w-full  bg-[#F6F6F7] relative bottom-0">
-      <div className=" min-w-[240px] max-w-[1230px]  mx-auto relative">
-       <Footer/>
-      </div>
-    </div>
-    </div>
-   }
-  }
-  else
-  {
-      return <div className="relative flex flex-col ">
-
-      <div className="w-full z-10 bg-white sticky top-0 ">
-      <div className=" min-w-[240px] max-w-[1230px] mx-auto ">
-      <Header/>
-      </div>
-      </div>
-      <p className='text-center font-extrabold '>Loading...</p>
-      <div className="w-full  bg-[#F6F6F7] relative bottom-0">
-        <div className=" min-w-[240px] max-w-[1230px]  mx-auto relative">
-         <Footer/>
+        </InitialAllBlogsContext.Provider>
+      );
+    } else {
+      return (
+        <div className="relative flex flex-col h-full ">
+          <div className="w-full z-10 bg-white sticky top-0 ">
+            <div className=" min-w-[240px] max-w-[1230px] mx-auto ">
+              <Header />
+            </div>
+          </div>
+          <p className="text-center font-extrabold ">No internet connection</p>
+          <div className="w-full  bg-[#F6F6F7] relative bottom-0">
+            <div className=" min-w-[240px] max-w-[1230px]  mx-auto relative">
+              <Footer />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  } else {
+    return (
+      <div className="relative flex flex-col ">
+        <div className="w-full z-10 bg-white sticky top-0 ">
+          <div className=" min-w-[240px] max-w-[1230px] mx-auto ">
+            <Header />
+          </div>
+        </div>
+        <div className="mt-10 text-center">
+          <span class="loading loading-dots loading-lg "></span>
         </div>
       </div>
-      </div>
+    );
   }
 }
