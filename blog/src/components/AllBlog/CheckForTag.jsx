f5d13d3feb47033edacd24c4ec69346df0f712a6
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-20 17:22:41 +0800

    dialog window add event listener is added

--- blog/src/components/AllBlog/CheckForTag.jsx
+++ blog/src/components/AllBlog/CheckForTag.jsx
@@ -1,26 +1,41 @@
-export const CheckForTag=({setAllTags,allTags,tag})=>{
-  return <div className="w-full h-fit grid grid-cols-3 min-[480px]:justify-items-center  *:text-xs ">
-  <p  className="w-[18%] flex items-center flex-wrap ">Tag:<span className="font-bold text-xs leading-6">{tag.title?.split('').map((l,i)=>{
-    if(i==0)
-    {
-      return l.toUpperCase(0)
-    }
-    return l
-  }).join('')}</span></p>
-  <p className="text-center">Included blogs:{' '}<span className="font-bold text-xs leading-6">{tag.included_blog}</span></p>
-    <label className="text-center font-bold text-xs leading-6">
-    <input type="checkbox" checked={tag.included} onChange={(e)=>{
-        setAllTags(allTags.map(t=>{
-            if(t.id===tag.id)
-            {
-                return {...tag,included:e.target.checked}
-            }
-            else
-            {
-                return t
-            }
-        }))
-    }}/> Check tag
-  </label>
-  </div>
-}
+export const CheckForTag = ({ setAllTags, allTags, tag }) => {
+  return (
+    <div className="w-full h-fit grid grid-cols-3 min-[480px]:justify-items-center  *:text-xs ">
+      <p className="w-[18%] flex items-center flex-wrap ">
+        <span className="font-bold text-xs leading-6">
+          {tag.title
+            ?.split("")
+            .map((l, i) => {
+              if (i == 0) {
+                return l.toUpperCase(0);
+              }
+              return l;
+            })
+            .join("")}
+        </span>
+      </p>
+      <p className="text-center">
+        Included blogs:{" "}
+        <span className="font-bold text-xs leading-6">{tag.included_blog}</span>
+      </p>
+      <label className="text-center font-bold text-xs leading-6">
+        <input
+          type="checkbox"
+          checked={tag.included}
+          onChange={(e) => {
+            setAllTags(
+              allTags.map((t) => {
+                if (t.id === tag.id) {
+                  return { ...tag, included: e.target.checked };
+                } else {
+                  return t;
+                }
+              })
+            );
+          }}
+        />{" "}
+        Check tag
+      </label>
+    </div>
+  );
+};
--- blog/src/components/AllBlog/TagEditing.jsx
+++ blog/src/components/AllBlog/TagEditing.jsx
@@ -1,5 +1,5 @@
 import { CheckForTag } from "./CheckForTag";
-import { useState, forwardRef } from "react";
+import { useState, forwardRef, useEffect } from "react";
 export const TagEditing = forwardRef(function TagEditing(props, ref) {
   const { allTags, setAllTags } = props;
   let allTagsNotForState = [...allTags];
@@ -7,6 +7,15 @@ export const TagEditing = forwardRef(function TagEditing(props, ref) {
   const [isSortingAlphabet, setIsSortingAlphabet] = useState(false);
   const [tagSearchText, setTagSearchText] = useState("");
   //for tags editing
+  useEffect(() => {
+    function closeDialog() {
+      ref.current.close();
+    }
+    window.addEventListener("click", closeDialog);
+    return () => {
+      window.removeEventListener("click", closeDialog);
+    };
+  }, []);
   if (isSortingAlphabet) {
     allTagsNotForState.sort((a, b) => {
       if (a.title < b.title) {
@@ -25,7 +34,12 @@ export const TagEditing = forwardRef(function TagEditing(props, ref) {
   }
   return (
     <dialog className="modal" ref={ref}>
-      <ul className="flex flex-col gap-2 items-center relative w-[80%] max-h-[600px] mx-auto rounded-2xl border-t-2 border-t-[#D4A373] border-b-2 border-b-[#D4A373]   overflow-y-scroll   shadow-lg modal-box">
+      <ul
+        className="flex flex-col gap-2 items-center relative w-[80%] max-h-[600px] mx-auto rounded-2xl border-t-2 border-t-[#D4A373] border-b-2 border-b-[#D4A373]   overflow-y-scroll   shadow-lg modal-box"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
         <p className="font-bold text-xs leading-6 text-white bg-[#D4A373] border-t-2 border-t-white px-8 text-center sticky top-0 rounded-xl">
           Tags editing
         </p>
